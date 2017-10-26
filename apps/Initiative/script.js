f5c932bcf75da1ac0a3be9316a35b5fe27b3a9a6
Pasi Lampinen <plam@fl027.local>
2017-10-26 23:25:13 +0300

    Dynamic titles

--- apps/Initiative/script.js
+++ apps/Initiative/script.js
@@ -1,7 +1,19 @@
 
-var isFormTouched = false;
 
-function touchForm() {
+var titles = {
+  IDEA: 'What kind of idea do you have?',
+  GOOD: 'What has gone well?',
+  NOT_GOOD: 'What could be improved?',
+  FACT: 'Tell us what you know',
+}
+
+var isFormTouched = false;
+function touchForm(value) {
+  var question = document.getElementById('question-label');
+  if (question) {
+    question.innerHTML = titles[value] || 'How do you feel?';
+  }
+
   if (isFormTouched) {
     return;
   }
